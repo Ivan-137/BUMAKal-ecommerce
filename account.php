8d315d2e7d3116bf6747cecd33a4c76b77a66c85
Ivan-137 <157514871+Ivan-137@users.noreply.github.com>
2024-06-02 23:54:12 +0800

    Add files via upload

---
+++ account.php
@@ -0,0 +1,257 @@
+<?php
+
+include('server/connection.php');
+session_start();
+
+if(!isset($_SESSION['logged_in'])){
+    header('location: login.php');
+    exit;
+}
+
+if(isset($_GET['logout'])){
+    if(isset($_SESSION['logged_in'])){
+    unset($_SESSION['logged_in']);
+    unset($_SESSION['username']);
+    unset($_SESSION['user_name']);
+    header('location: login.php');
+    exit;
+    }
+}
+
+
+if(isset($_POST['change_password'])){
+
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirm_password'];
+    $username = $_SESSION['username'];
+    
+    //if passwords don't match
+    if($password !== $confirmPassword){
+        header('location: account.php?error=passwords do not match');
+    //if password is less than 6 characters
+    } else if(strlen($password) < 6){
+        header('location: account.php?error=password must be at least 6 characters');
+     //no errors
+    }else{
+
+        $stmt = $conn->prepare("UPDATE users SET user_password=? WHERE username=?");
+        $stmt->bind_param('ss', md5($password), $username);
+
+        if($stmt->excute()){
+            header('location: account.php?message=password has been updated succesfully');
+        }else{
+            header('location: account.php?error=could not update password');
+        }
+        
+    }
+}
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="assets/css/style.css"/>
+
+
+</head>
+<body>
+
+    <!--Navbar-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-skyblue py-3 fixed-top">
+        <div class="container">
+          <img class="logo" src="assets/imgs/logoo.png"/>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="shop.html">Shop</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">Blog</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact Us</a>
+              </li>
+
+
+              <li class="nav-item">
+                <a class="nav-link" href="cart.php">Cart</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="account.html">Account</a>
+              </li>
+
+
+
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+    <!--Account-->
+    <section class="my-5 py-5">
+        <div class="row container mx-auto">
+            <div class="text-center mt-3 pt-5 col-lg-6 col-md-12 col-sm-12">
+            <p class="text-center"style="color:green"><?php if(isset($_GET['register_success'])){ echo $_GET['register_success'];}?></p>
+            <p class="text-center"style="color:green"><?php if(isset($_GET['login_success'])){ echo $_GET['login_success'];}?></p>
+                <h3 class="font-weight-bold">Account Info</h3>
+                <p class="text-center"style="color:red"><?php if(isset($_GET['error'])){ echo $_GET['error'];}?></p>
+                <p class="text-center"style="color:green"><?php if(isset($_GET['message'])){ echo $_GET['error'];}?></p>
+                <hr class="mx-auto"> 
+                <div class="account-info">
+                    <p>Name <span>Ivan</span></p>
+                    <p>Email <span>ivan@gmail.com</span></p>
+                    <p><a href="" id="orders-btn">Your Orders</a></p>
+                    <p><a href="" id="logout-btn">Logout</a></p>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-12 col-sm-12">
+                <form id="account-form" method="POST" action=account.php>
+                    <h3>Change Password</h3>
+                    <hr class="mx-auto"/>
+
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" id="account-password" name="password" placeholder="Password" required/>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Confirm Password</label>
+                        <input type="password" class="form-control" id="account-password-confirm" name="confirmPassword" placeholder="Password" required/>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Change Password" name="change_password"  class="btn" id="change-pass-btn"/>
+
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </section>
+
+    <!--Orders-->
+    <section class="orders container my-5 py-3">
+        <div class="container mt-2">
+            <h2 class="font-weight-bold text-center">Your Orders</h2>
+ 
+        </div>
+
+        <table class="mt-5 pt-5">
+            <tr>
+                <th>Product</th>
+                <th>Date</th>
+            </tr>
+            <tr>
+                <td>
+                    <div class="product-info">
+                        <img src="assets/imgs/BUP_Lanyard2.png"/>    
+                        <div>
+                            <p class="mt-3">BUP Lanyard</p>
+                        </div>
+                    </div>
+                </td>
+            
+                <td>
+                    <span>01-03-2024</span>
+                </td>
+            </tr>
+
+
+        </table>
+
+
+            
+    </section>
+
+
+
+      <!--Footer-->
+      <footer class="mt-5 py-5">
+        <div class="row container mx-auto pt-5">
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <img class="logo" src="assets/imgs/logoo.png"/>
+                <p class="pt-3">We provide the best product for the most affordable prices</p>
+            </div>
+    
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <h5 class="pb-2">Featured</h5>
+                <ul class="text-uppercase">
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Lanyards</a></li>
+                    <li><a href="#">Totebags</a></li>
+                    <li><a href="#">Stickers</a></li>
+                    <li><a href="#">New Release</a></li>
+                </ul>
+            </div>
+    
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <h5 class="pb-2">Contact Us</h5>
+                <div>
+                    <h6 class="text-uppercase">Address:</h6>
+                    <p>Rizal Street, Legazpi City, Albay, Legazpi, Philippines</p>
+                </div>
+                <div>
+                    <h6 class="text-uppercase">Contact Number:</h6>
+                    <p>090909090909 / 09324142342</p>
+                </div>
+                <div>
+                    <h6 class="text-uppercase">Email Address:</h6>
+                    <p>admin@bicol-u.edu.ph</p>
+                </div>
+            </div>
+    
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12 ">
+                <h5 class="pb-2">Instagram</h5>
+                <div class="row">
+                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
+                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
+                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
+                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
+                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
+                </div>
+            </div>
+        </div>
+    
+        <div class="copyright mt-5">
+            <div class="row container mx-auto">
+                <div class="col-lg-3 col-md-5 col-sm-12 mb-4">
+                    <img src="assets/imgs/payment.png"/>
+                    <img src="assets/imgs/Gcash.png"/>
+                </div>
+                <div class="col-lg-3 col-md-5 col-sm-12 mb-4 text-nowrap mb-2">
+                    <p>BUMAKAL@2024 All Right Reserved</p>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 mb-4"> 
+                    <a href="#"><i class="fab fa-facebook"></i> </a>
+                    <a href="#"><i class="fab fa-instagram"></i> </a>
+                    <a href="#"><i class="fab fa-x"></i> </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
